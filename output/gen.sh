f0bcf5a1a454839384432a2cdd6013e38b3df576
Gaohan Miao <gaohanm@gmail.com>
2019-03-20 11:56:49 -0400

    save

--- output/gen.sh
+++ output/gen.sh
@@ -14,28 +14,28 @@ rm *.out
 
 for A in $Alist; do
 for p in $plist; do
-    cat scan_p_ex_raw/e2.A$A.s1.00.px$p.out >> e.A$A.scan_p.out
-    cat scan_p_fssh_raw/f2.A$A.s1.00.px$p.out >> f.A$A.scan_p.out
-    cat scan_p_fssh_phase_corr_raw/fc2.A$A.s1.00.px$p.out >> fc.A$A.scan_p.out
+    cat scan_p_ex_raw/e.A$A.s1.00.px$p.out >> e.A$A.scan_p.out
+    cat scan_p_fssh_raw/f.A$A.s1.00.px$p.out >> f.A$A.scan_p.out
+    cat scan_p_fssh_phase_corr_raw/fc.A$A.s1.00.px$p.out >> fc.A$A.scan_p.out
 
-    cat scan_p_ex_model2_raw/e2.A$A.s1.00.px$p.out >> e.A$A.scan_p.model2.out
-    cat scan_p_fssh_model2_raw/f2.A$A.s1.00.px$p.out >> f.A$A.scan_p.model2.out
-    cat scan_p_fssh_model2_phase_corr_raw/f2.A$A.s1.00.px$p.out >> fc.A$A.scan_p.model2.out
+    cat scan_p_ex_model2_raw/e.A$A.s1.00.px$p.out >> e.A$A.scan_p.model2.out
+    cat scan_p_fssh_model2_raw/f.A$A.s1.00.px$p.out >> f.A$A.scan_p.model2.out
+    cat scan_p_fssh_model2_phase_corr_raw/fc.A$A.s1.00.px$p.out >> fc.A$A.scan_p.model2.out
 done
 done
 
 for A in $Alist; do
 for s in $slist; do
-    cat scan_s_ex_raw/e2.A$A.s$s.px30.out >> e.A$A.scan_s.out
-    cat scan_s_fssh_raw/f2.A$A.s$s.px30.out >> f.A$A.scan_s.out
-    cat scan_s_fssh_phase_corr_raw/fc2.A$A.s$s.px30.out >> fc.A$A.scan_s.out
+    cat scan_s_ex_raw/e.A$A.s$s.px30.out >> e.A$A.scan_s.out
+    cat scan_s_fssh_raw/f.A$A.s$s.px30.out >> f.A$A.scan_s.out
+    cat scan_s_fssh_phase_corr_raw/fc.A$A.s$s.px30.out >> fc.A$A.scan_s.out
 
-    cat scan_s_ex_model2_raw/e2.A$A.s$s.px30.out >> e.A$A.scan_s.model2.out
-    cat scan_s_fssh_model2_raw/f2.A$A.s$s.px30.out >> f.A$A.scan_s.model2.out
-    cat scan_s_fssh_model2_phase_corr_raw/f2.A$A.s$s.px30.out >> fc.A$A.scan_s.model2.out
+    cat scan_s_ex_model2_raw/e.A$A.s$s.px30.out >> e.A$A.scan_s.model2.out
+    cat scan_s_fssh_model2_raw/f.A$A.s$s.px30.out >> f.A$A.scan_s.model2.out
+    cat scan_s_fssh_model2_phase_corr_raw/fc.A$A.s$s.px30.out >> fc.A$A.scan_s.model2.out
 
-    cat scan_s_ehrenfest_raw/eh2.A$A.s$s.px30.out >> eh.A$A.scan_s.out
-    cat scan_s_ehrenfest_model2_raw/eh2.A$A.s$s.px30.out >> eh.A$A.scan_s.model2.out
+    cat scan_s_ehrenfest_raw/eh.A$A.s$s.px30.out >> eh.A$A.scan_s.out
+    cat scan_s_ehrenfest_model2_raw/eh.A$A.s$s.px30.out >> eh.A$A.scan_s.model2.out
 done
 done
 
